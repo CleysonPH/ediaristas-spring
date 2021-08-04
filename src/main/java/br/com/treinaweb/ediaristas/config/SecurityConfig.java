4775272b3cee8be715d99b3df53bd54e58928723
Cleyson Lima <cleysonph@gmail.com>
2021-08-04 13:37:12 -0300

    muda o AdminLTE para ser instalado via WebJars

--- src/main/java/br/com/treinaweb/ediaristas/config/SecurityConfig.java
+++ src/main/java/br/com/treinaweb/ediaristas/config/SecurityConfig.java
@@ -54,10 +54,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
-            .antMatchers("/adminlte/**")
+            .antMatchers("/webjars/**")
             .antMatchers("/img/**")
-            .antMatchers("/js/**")
-            .antMatchers("/plugins/**");
+            .antMatchers("/js/**");
     }
 
 }
